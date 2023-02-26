1d7c801f72c757ed7823589d839d826c56dad8bb
Seth McCullough <seth@endevrs.dev>
2023-02-26 14:53:12 -0500

    Setup theme provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1>AI Crosswords</h1>
+      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl text-center mt-1">
+        AI Crosswords
+      </h1>
     </main>
-  );
+  )
 }
