8c352cb6bfa44f64521a5061f2e76e935098eff7
Seth McCullough <seth@endevrs.dev>
2023-02-27 14:53:25 -0500

    Add plausible provider for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Inter as FontSans } from "next/font/google"
 
 import "@/styles/globals.css"
+import PlausibleProvider from "next-plausible"
+
 import { cn } from "@/lib/utils"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -23,24 +25,25 @@ export const metadata = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <body
-          className={cn(
-            "min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50",
-            fontSans.variable
-          )}
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <PlausibleProvider domain="ai-crossword.vercel.app" />
+      </head>
+      <body
+        className={cn(
+          "min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          disableTransitionOnChange
         >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            disableTransitionOnChange
-          >
-            {children}
-            <TailwindIndicator />
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
+          {children}
+          <TailwindIndicator />
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }
