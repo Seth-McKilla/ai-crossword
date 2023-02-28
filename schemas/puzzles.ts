3c6f377b3efa1ec3f4278773d3789a26965bd7b3
Seth McCullough <seth@endevrs.dev>
2023-02-27 22:48:00 -0500

    Add validation to ensure all answers max 20 characters

--- schemas/puzzles.ts
+++ schemas/puzzles.ts
@@ -5,12 +5,23 @@ import { stringListToArray } from "@/lib/utils"
 export const puzzlePropertiesSchema = z.object({
   readingLevel: z.string(),
   theme: z.string(),
-  answers: z.string().refine(
-    (value) => {
-      const answers = stringListToArray(value)
-      return answers.length >= 5 && answers.length <= 30
-    },
-    { message: "Must have between 5 and 30 answers" }
-  ),
+  answers: z
+    .string()
+    .refine(
+      (value) => {
+        const answers = stringListToArray(value)
+        return answers.length >= 5 && answers.length <= 30
+      },
+      { message: "Must have between 5 and 30 answers" }
+    )
+    .refine(
+      (value) => {
+        const answers = stringListToArray(value)
+        return answers.every((answer) => answer.length <= 20)
+      },
+      {
+        message: "All answers must be 20 characters or less",
+      }
+    ),
 })
 export type PuzzleProperties = z.infer<typeof puzzlePropertiesSchema>
