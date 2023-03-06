70a473486749b1964732b1055ba9c396e7ad05a1
Cameron Merriman <cameron.a.merriman@gmail.com>
2023-03-05 22:30:24 -0500

    Rectified occasionally encountered bug. Added utility to write crossword dictionary to file as .json file. Added example to python/output/.

--- python/main.py
+++ python/main.py
@@ -1,19 +1,30 @@
 import random
 import json
+import os
 
-def main():
-    # INPUTS: 1.  N (int): -> Crossword Puzzle grid is N x N (square)
-    #         2.  answerBank (str) -> List of crossword answers
+def main(gridLength=20,answerList=['Elephant','Buffalo','Colorado','Panda','Bear','Engineer','Overwatch','Valheim','Jeopardy']):
+    # INPUTS: 1.  gridLength (int)          -> Crossword Puzzle grid is size x N (square)
+    #         2.  answerList (list of str)  -> List of crossword answers
     #
     # OUTPUTS 1.  crosswordGlossary (dict) -> A dictionary composed of KEYS (clue numeric index),
     #             and VALUES (lists) organized like [row index, col index, answer, orientation]
+    #             NOTE: The crosswordGlossary is written to file as crosswordGlossary.json
 
+    # Input validation:
+    # 1. Assert that the supplied gridLength is an integer. If assertion passes, assign int() to protect against float issues
+    # 2. Assert that the supplied answerList is a list composed of strings. Validation should be done external to Python to ensure no non-alphanumeric strings show up here (e.g., '2')
+    assert float(gridLength) and gridLength.__floor__() == gridLength, f"The main() 'gridLength' argument must be an integer. User-supplied value: {gridLength}"
+    gridLength = int(gridLength)
+    assert isinstance(answerList,list) and all([isinstance(element,str) for element in answerList]), f"The main() 'answerList' argument must be a Python list composed of string elements. User-supplied value: {answerList}"
 
-    N = 20
-    answerBank = ['Elephant','Buffalo','Colorado','Panda','Bear','Engineer','Overwatch','Valheim','Jeopardy']
-    
-    crossword, indexDict = generate_crossword(size=N,answerBank=answerBank)
+    # Make call to generate the 'crossword' (list) and 'indexDict' (used for .json output)
+    # NOTE: 'crossword' is only useful as a debugging tool, can be removed later
+    crossword, indexDict = generate_crossword(size=gridLength,answerBank=answerList)
 
+    # Once we have all placed crossword answers, generate a dictionary, 'crosswordGlossary', composed of the results:
+    # KEY:      The index of the crossword clue (i.e. the numeric value that shows up in the crossword box corresponding to the answer's first letter)
+    # VALUES:   [Row Index, Column Index, Corresponding Word, Orientation]
+    #           NOTE: The row & column indices begin at ZERO (not 1)
     [element.append('DOWN') for element in indexDict['DOWN']]
     [element.append('ACROSS') for element in indexDict['ACROSS']]
 
@@ -26,14 +37,20 @@ def main():
     for elementIdx, element in enumerate(tempList):
         crosswordGlossary[elementIdx+1] = element
 
-    json_string = json.dumps(crosswordGlossary)
-    print(json_string)
+    json_output = json.dumps(crosswordGlossary)
+    # Build output directory and store the crosswordGlossary.json to file
+    outputDirectory = os.path.abspath(os.path.join(os.path.dirname(__file__),'output'))
+    if not os.path.exists(outputDirectory):
+        os.mkdir(outputDirectory)
+    with open(os.path.abspath(os.path.join(outputDirectory,'crossword_glossary.json')),"w") as outfile:
+        outfile.write(json_output)
+
+    # DEBUG UTILS
+    #print(json_string)
     # Send out the glossary with json
-    for row in crossword:
-        print(' '.join(row))
+    #for row in crossword:
+    #    print(' '.join(row))
     
-
-
 def generate_crossword(size=20,answerBank=None):
     answerBank = [element.upper() for element in answerBank]
     skippedAnswerBank = list() # TODO
@@ -92,10 +109,15 @@ def word_can_be_placed(size, word, grid, rowStart, colStart, is_horizontal):
             if colStart + letterIdx not in range(size):
                 return False
 
-            # Ensure empty (or exterior) nodes to the left + right of the word
-            if (letterIdx == 0 and colStart - 1 >= 0 and grid[rowStart][colStart - 1] not in [' ']) \
-            or (letterIdx == len(word)-1 and colStart + letterIdx + 1 <= size and grid[rowStart][colStart + letterIdx + 1] not in [' ']):
-                return False
+            # Ensure empty (or exterior) nodes to the left + right of the word\
+            #if (letterIdx == 0 and colStart - 1 < 0) or (letterIdx == len(word)-1 and colStart + letterIdx + 1 > size):
+            #    return True
+            if letterIdx == 0 and colStart - 1 >= 0:
+                if grid[rowStart][colStart - 1] not in [' ']:
+                    return False
+            elif letterIdx == len(word)-1 and colStart + letterIdx + 1 <= size:
+                if grid[rowStart][colStart + letterIdx + 1] not in [' ']:
+                    return False
 
             # Ensure each letter is placed on an empty or equal-letter square
             if grid[rowStart][colStart+letterIdx] not in [' ',letter]:
@@ -112,9 +134,14 @@ def word_can_be_placed(size, word, grid, rowStart, colStart, is_horizontal):
                 return False
 
             # Ensure empty (or exterior) nodes to the top + bottom of the word
-            if (letterIdx == 0 and rowStart - 1 >= 0 and grid[rowStart - 1][colStart] not in [' ']) \
-            or (letterIdx == len(word)-1 and rowStart + letterIdx + 1 <= size and grid[rowStart+ letterIdx + 1][colStart] not in [' ']):
-                return False
+            #if (letterIdx == 0 and rowStart - 1 < 0) or (letterIdx == len(word)-1 and rowStart + letterIdx + 1 > size):
+            #    break
+            if letterIdx == 0 and rowStart - 1 >= 0:
+                if grid[rowStart - 1][colStart] not in [' ']:
+                    return False
+            elif letterIdx == len(word)-1 and rowStart + letterIdx + 1 <= size:
+                if grid[rowStart+ letterIdx + 1][colStart] not in [' ']:
+                    return False
 
             # Ensure each letter is placed on an empty or equal-letter square
             if grid[rowStart+letterIdx][colStart] not in [' ',letter]:
